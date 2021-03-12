af9f27b69563a82252eb8808544c4488eebf4b6c
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-12 13:01:32 +0900

    passwordのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /[a-z\d]{8,}/i, message: "英字と数字の両方を含めて設定してください" }
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+
 end
