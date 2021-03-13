f0a0d8861a9efa11e4777e578dcb2026fa176118
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-13 15:10:25 +0900

    itemモデルにバリデーションをかける

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :delivery_carge_id
+    validates :delivery_day_id
+    validates :prefecture_id
+  end
+
+  has_one_attached :image
+  belongs_to :user
+  has_one :order
+
 end
