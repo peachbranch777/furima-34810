f249e82a22c7b0a1239b15726b68cde2573c3c69
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-13 18:48:19 +0900

    itemモデルバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,28 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
+    validates :price, inclusion: { in: 300..9999999 }, format: {with: /\A[0-9]+\z/}
     validates :category_id
     validates :condition_id
-    validates :delivery_carge_id
+    validates :delivery_charge_id
     validates :delivery_day_id
     validates :prefecture_id
+    validates :image
   end
+
+
 
   has_one_attached :image
   belongs_to :user
   has_one :order
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_day
+  belongs_to :prefecture
+
+  validates :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id, numericality: { other_than: 1 } 
 
 end
