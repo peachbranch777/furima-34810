316ecd1f2af9f7bb7244a8aaf180459316d71d9f
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-13 21:01:50 +0900

    ストロングパラメーターに imageカラム追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id, ).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id, :image).merge(user_id: current_user.id)
   end
 end
